57e2f038890095fcb8536658b86b0608085a031b
PreciseAardvark <abraha.delarosaaguayo.509@gmail.com>
2025-05-13 15:16:08 -0600

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://github.com/marketing-beyond/',
   vite: {
     plugins: [tailwindcss()]
   },
