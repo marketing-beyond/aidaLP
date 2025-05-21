6d01872d16b5d54a705492eb9b1ad43f43147624
Abraham De La Rosa <abraham.delarosa@MacBook-Pro-de-Abraham.local>
2025-05-21 10:21:34 -0600

    feature: add analytics, change meta description, add robots.txt and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://github.com/marketing-beyond/',
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [icon()],
+  integrations: [icon(), sitemap()],
 });
